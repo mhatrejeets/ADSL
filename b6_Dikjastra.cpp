55f24dd25b5549871f0e3ead03df2fc0e48959ac
mhatrejeets <166438594+mhatrejeets@users.noreply.github.com>
2024-05-06 06:11:20 +0530

    Update b6_Dikjastra.cpp

--- b6_Dikjastra.cpp
+++ b6_Dikjastra.cpp
@@ -26,49 +26,55 @@ public:
     }
 
     void dijkstra()
+{
+    int visited[10] = {}, pre[10], distance[10], count, nextnode, min = 999;
+
+    // Initialization
+    for (int i = 0; i < vertices; i++)
     {
-        int visited[10] = {}, pre[10], distance[10], count, nextnode, min = 999;
-
-        for (int i = 0; i < vertices; i++)
-        {
-            distance[i] = g[0][i];
-            pre[i] = 0;
-        }
-
-        distance[0] = 0;
-        visited[0] = 1;
-        count = 1;
-
-        while (count < vertices - 1)
-        {
-            min = 999;
-            for (int i = 0; i < vertices; i++)
-            {
-                if (distance[i] < min && visited[i] == 0)
-                {
-                    min = distance[i];
-                    nextnode = i;
-                }
-            }
-
-            visited[nextnode] = 1;
-            for (int i = 0; i < vertices; i++)
-            {
-                if ((distance[i] > min + g[nextnode][i]) && visited[i] != 1)
-                {
-                    distance[i] = min + g[nextnode][i];
-                    pre[i] = nextnode;
-                }
-            }
-            count++;
-        }
-
-        for (int i = 0; i < vertices; i++)
-        {
-            cout << "\nDistance from 0 to " << i << ": ";
-            cout << distance[i];
-        }
+        distance[i] = g[0][i];
+        pre[i] = 0;
     }
+
+    distance[0] = 0;
+    visited[0] = 1;
+    count = 1;
+
+    // Dijkstra's Algorithm
+    while (count < vertices - 1)
+    {
+        min = 999;
+        // Find the next node with the minimum distance
+        for (int i = 0; i < vertices; i++)
+        {
+            if (distance[i] < min && visited[i] == 0)
+            {
+                min = distance[i];
+                nextnode = i;
+            }
+        }
+        // Mark the selected node as visited
+        visited[nextnode] = 1;
+        // Update the distance array
+        for (int i = 0; i < vertices; i++)
+        {
+            if ((distance[i] > min + g[nextnode][i]) && visited[i] != 1)
+            {
+                distance[i] = min + g[nextnode][i];
+                pre[i] = nextnode;
+            }
+        }
+        count++;
+    }
+
+    // Print the shortest distances from source (vertex 0) to all other vertices
+    for (int i = 0; i < vertices; i++)
+    {
+        cout << "\nDistance from 0 to " << i << ": ";
+        cout << distance[i];
+    }
+}
+
 };
 
 int main()
